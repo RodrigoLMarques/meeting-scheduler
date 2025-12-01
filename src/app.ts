4d021721d05eb85e26fa0f97c302c18c8bd75643
RodrigoLMarques <rodrigolopes.m2005@gmail.com>
2025-12-01 17:48:29 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import { errorHandler } from "./middlewares/errorHandler";
 import { routes } from "./routes";
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://marcae.marquesdev.com"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", routes);
 app.use(errorHandler);
